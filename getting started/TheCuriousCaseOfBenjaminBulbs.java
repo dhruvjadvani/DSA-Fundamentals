5e65853126a43ac809a4f8f9181e2349caff873d
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-01 12:45:27 +0530

    Update TheCuriousCaseOfBenjaminBulbs.java

--- getting started/TheCuriousCaseOfBenjaminBulbs.java
+++ getting started/TheCuriousCaseOfBenjaminBulbs.java
@@ -5,10 +5,10 @@ import java.util.*;
   public static void main(String[] args) {
     // write your code here  
     Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
+    int n = scn.nextInt();
 		
-		for(int i = 1; i * i <= n; i++) {
-			System.out.println(i * i);
-		}
+    for(int i = 1; i * i <= n; i++) {
+           System.out.println(i * i);
+    }
    }
   }
