6e16cc5629d56b6617de662ea7d0e36101945a96
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-01 22:09:19 +0530

    Create PatternThree.java

---
+++ patterns/PatternThree.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class patternThree {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int space = n - 1;
+        int star = 1;
+        
+        
+        for(int i = 1; i <= n; i++){
+            //System.out.println(space + ", " + st);
+ 			for(int j = 1; j <= space; j++) {
+ 				System.out.print("\t");
+ 			}
+ 			
+ 			for(int j = 1; j <= star; j++) {
+ 				System.out.print("*\t");
+ 			}
+      
+        	space--;
+ 			star++;
+            System.out.println(); 
+        }
+        
+	}
+
+}
+
+        *	
+          
+			*	*	
+
+		*	*	*	
+
+	*	*	*	*	
+
+*	*	*	*	*
+
