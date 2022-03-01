b2d79567051485be2ec2297070879ced07c811a4
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-01 09:49:24 +0530

    Create PrimeFactorizationOfNum.java

---
+++ getting started/PrimeFactorizationOfNum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class PrimeFactorisationOfANumber {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		int num = scn.nextInt(); 
+		
+		for(int div = 2; div * div <= num; div++) { 
+			while(num % div == 0) { 
+				num = num / div;   
+				System.out.println(div + " "); 
+			}
+		}
+		
+		if(num != 1) {
+			System.out.println(num);
+		}
+	}
+
+}
