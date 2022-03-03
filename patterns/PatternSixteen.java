b1efc8036138b6fc317c2cdf3e77f3d548743ada
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-04 02:59:31 +0530

    :)

---
+++ patterns/PatternSixteen.java
@@ -0,0 +1,37 @@
+package basicOfProgramming.patterns;
+import java.util.*;
+public class PatternSixteen {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		int n = scn.nextInt(); 
+		int space = 2 * n - 3;  
+		int star = 1;
+		
+		for(int i = 1; i <= n; i++) { 
+			int val = 1;
+			for(int j = 1; j <= star; j++) { 
+				System.out.print(val +"\t");
+				val++;
+			}
+			for(int j = 1; j <= space; j++) { 
+				System.out.print("\t");
+			}
+			
+			if(i == n) {  
+				star--;
+				val--; 
+			}
+			for(int j = 1; j <= star; j++) { 
+				val--; 
+				System.out.print(val +"\t");
+			}
+			
+			star++; 
+			space = space - 2; 
+			System.out.println();
+		}
+	}
+
+}
