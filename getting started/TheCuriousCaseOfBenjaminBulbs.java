31e31d0b3d2d374f9dd92564a97cd122a5930b3f
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-10 00:30:06 +0530

    :)

--- getting started/TheCuriousCaseOfBenjaminBulbs.java
+++ getting started/TheCuriousCaseOfBenjaminBulbs.java
@@ -7,7 +7,7 @@ import java.util.*;
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
 		
-    for(int i = 1; i * i <= n; i++) {
+    for (int i = 1; i * i <= n; i++) {
            System.out.println(i * i);
     }
    }
