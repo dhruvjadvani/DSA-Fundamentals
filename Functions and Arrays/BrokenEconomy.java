731d4652d28c18134f0e3bdc287aa2b504ea6a33
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-09 20:19:53 +0530

    :)

---
+++ Functions and Arrays/BrokenEconomy.java
@@ -0,0 +1,41 @@
+package basicOfProgramming.Arrays;
+import java.util.*;
+public class BrokenEconomy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		
+		int data = scn.nextInt();
+		
+		int low = 0;
+		int high = arr.length - 1;
+		int ceil = 0;
+		int floor = 0;
+		
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if(data < arr[mid]) {
+				high = mid - 1;
+				ceil = arr[mid];
+			} else if(data > arr[mid]) {
+				low = mid + 1;
+				floor = arr[mid];
+			} else {
+				ceil = arr[mid];
+				floor = arr[mid];
+				break;
+			}
+		}
+		
+		System.out.println(ceil);
+		System.out.println(floor);
+	}
+
+}
