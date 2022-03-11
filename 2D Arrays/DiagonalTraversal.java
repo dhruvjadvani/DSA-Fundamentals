186f80f4b12e36701818f4ff3ca5b05685124734
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-11 23:43:00 +0530

    :)

---
+++ 2D Arrays/DiagonalTraversal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class DiagonalTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt(); 
+		int[][] arr = new int[n][n];
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = scn.nextInt();
+			}
+		}
+		
+		for(int gap = 0; gap <= arr.length; gap++) {
+			for(int i = 0, j = gap; j < arr.length; i++, j++) {
+				System.out.println(arr[i][j]);
+				
+			}
+		}
+	}
+
+}
