7164e2ba4fcfd5c84b49f5c350c7128d4bb63978
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-17 01:16:24 +0530

    :)

---
+++ Recursion/Intro to Recursion/RecursionFlow.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class RecursionFlow {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		pzz(n);
+	}
+	
+public static void pzz(int n){
+        if(n == 0) {
+        	return;
+        }
+        
+        System.out.println("pre " + n); 
+        pzz(n - 1);                     
+        System.out.println("In " + n);  
+        pzz(n - 1);                     
+        System.out.println("Post" + n); 
+        
+    }
+
+}
