a4198eeb1631dfada5804a10e40cd07816ccfb45
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-20 23:45:55 +0530

    :)

---
+++ Recursion/Recursion in Arrays/FirstIndex.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class FirstIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		
+		int d = scn.nextInt();
+		int fi = firstIndex(arr, 0, d);
+		System.out.println(fi);
+	}	
+		 public static int firstIndex(int[] arr, int idx, int x){
+		        if(idx == arr.length) {
+		        	return -1;
+		        }
+		        
+		        int fiisa = firstIndex(arr, idx + 1, x); 
+		        if(arr[idx] == x) {
+		        	return idx;
+		        } else {
+		        	return fiisa;
+		        }
+		    }
+	}
+
+
