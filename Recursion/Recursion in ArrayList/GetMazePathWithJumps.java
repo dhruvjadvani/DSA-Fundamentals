334c05a8cc471a045a07bc69c41b40296dc93d19
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-03-23 20:26:19 +0530

    :)

---
+++ Recursion/Recursion in ArrayList/GetMazePathWithJumps.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class GetMazePathsWithJumps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int m = scn.nextInt();
+		ArrayList<String> paths = getMazePaths(1, 1, n, m);
+		System.out.println(paths);
+	}
+	
+	
+	public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        if(sr == dr && sc == dc) {
+        	ArrayList<String> bres = new ArrayList<>();
+        	bres.add("");
+        	return bres;
+        }
+        
+        
+        ArrayList<String> paths = new ArrayList<>();
+        
+        
+        
+        for(int ms = 1; ms <= dc - sc; ms++) { 
+        	 ArrayList<String> hpaths = getMazePaths(sr, sc + ms, dr, dc);
+        	 for(String hpath: hpaths) {
+        		 paths.add("h" + ms + hpath);
+        	 }	 
+        }
+        
+     
+        for(int ms = 1; ms <= dr - sr; ms++) { 
+        	 ArrayList<String> vpaths = getMazePaths(sr + ms, sc, dr, dc);
+        	 for(String vpath: vpaths) {
+        		 paths.add("v" + ms + vpath);
+        	 }	 
+        }
+        
+      
+        for(int ms = 1; ms <= dr - sr && ms <= dc - sc; ms++) { 
+        	 ArrayList<String> dpaths = getMazePaths(sr + ms, sc + ms, dr, dc);
+        	 for(String dpath: dpaths) {
+        		 paths.add("d" + ms + dpath);
+        	 }	 
+        }
+        
+        return paths;
+    }
+
+}
