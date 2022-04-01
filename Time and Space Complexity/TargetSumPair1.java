ba496e6d31e41030b0d71599fd031813fdd1d24a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-01 15:45:48 +0530

    :)

--- Time and Space Complexity/TargetSumPair1.java
+++ Time and Space Complexity/TargetSumPair1.java
@@ -1,1 +1,32 @@
+import java.util.*;
+public class TargetSumPair {
+	public static void targetSumPair(int[] arr, int target){
+	    //write your code here
+		Arrays.sort(arr); 
+		int leftIndex = 0;
+		int rightIndex = arr.length - 1;
+		while(leftIndex < rightIndex) {
+			if(arr[leftIndex] + arr[rightIndex] < target) {
+				leftIndex++;
+			} else if(arr[leftIndex] + arr[rightIndex] > target) {
+				rightIndex--;
+			} else { 
+				System.out.println(arr[leftIndex] + ", " + arr[rightIndex]);
+				leftIndex++;
+				rightIndex--;
+			}
+		}
+	  }
+	  public static void main(String[] args) throws Exception {
+	    Scanner scn = new Scanner(System.in);
+	    int n = scn.nextInt();
+	    int[] arr = new int[n];
+	    for(int i = 0 ;i < n; i++){
+	      arr[i] = scn.nextInt();
+	    }
+	    int target = scn.nextInt();
+	    targetSumPair(arr,target);
+	  }
+
+}
 
