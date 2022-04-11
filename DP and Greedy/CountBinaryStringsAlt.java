d9cdd01d7557a83f10a7d25478fc74d84a064d22
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-11 13:06:57 +0530

    :)

--- DP and Greedy/CountBinaryStringsAlt.java
+++ DP and Greedy/CountBinaryStringsAlt.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+public class CountBinaryStringsAlt {
+	public static void main(String[] args) throws Exception {
+	    // write your code here
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		int oczeros = 1;
+		int ocones = 1;
+		
+		for(int i = 2; i <= n; i++) {
+			int nczeros = ocones;
+			int ncones = oczeros + ocones;
+			
+			ocones = ncones;
+			oczeros = nczeros;
+			
+		}
+		
+		System.out.println(oczeros + ocones);
+		
+	}
+}
 
