e1375333e1623590b7fae6cd2c5c5f742dba52bd
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-11 13:05:47 +0530

    :)

--- DP and Greedy/CountBinaryStrings.java
+++ DP and Greedy/CountBinaryStrings.java
@@ -1,1 +1,23 @@
 
+import java.util.*;
+public class CountBinaryStrings {
+	public static void main(String[] args) throws Exception {
+	    // write your code here
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		int[] dp0 = new int[n + 1];
+		int[] dp1 = new int[n + 1];
+		
+		dp0[1] = 1;
+		dp1[1] = 1;
+		
+		for(int i = 2; i < dp1.length; i++) {
+			dp1[i] = dp1[i - 1] + dp0[i - 1];
+			dp0[i] = dp1[i - 1];
+		}
+		
+		System.out.println(dp1[n] + dp0[n]);
+	 }
+}
+
