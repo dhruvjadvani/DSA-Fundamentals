f2c462912f1d127f10375c5cd3d44f0fe0d591d0
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-04-12 21:30:46 +0530

    :)

--- DP and Greedy/ArrangeBuildings.java
+++ DP and Greedy/ArrangeBuildings.java
@@ -1,1 +1,28 @@
+import java.io.*;
+import java.util.*;
 
+public class ArrangeBuildings{
+
+	public static void main(String[] args) throws Exception {
+	    // write your code here
+		Scanner scn = new Scanner(System.in);
+		long n = scn.nextInt();
+		
+		long oldBuildings = 1;
+		long oldSpaces = 1;
+		
+		for(long i = 2; i <= n; i++) {
+			long newBuilding = oldSpaces;
+			long newSpace = oldSpaces + oldBuildings;
+			
+			oldSpaces = newSpace;
+			oldBuildings = newBuilding;
+		}
+		
+		long total = oldSpaces + oldBuildings;
+		total = total * total;
+		
+		System.out.println(total);
+	 }
+
+}
