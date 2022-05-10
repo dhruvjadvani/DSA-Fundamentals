581a50430cb46851543eed0e3ab11ab8c9909352
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-05-10 22:26:43 +0530

    :)

--- DP and Greedy/PartitionIntoSubsets.java
+++ DP and Greedy/PartitionIntoSubsets.java
@@ -1,34 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-   public static void main(String[] args) throws Exception {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      int k = Integer.parseInt(br.readLine());
-
-      if (n == 0 || k == 0 || n < k) {
-         System.out.println(0);
-         return;
-      }
-
-      long[][] dp = new long[k + 1][n + 1];
-      for (int i = 1; i <= k; i++) {
-         for (int j = i; j <= n; j++) {
-            if (i == 1 || j == 1 || i == j) {
-               dp[i][j] = 1;
-            } else {
-               dp[i][j] = dp[i - 1][j - 1] + i * dp[i][j - 1];
-            }
-         }
-      }
-
-      System.out.println(dp[k][n]);
-   }
-
+public class PartitionIntoSubsets {
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int k = scn.nextInt();
+		
+		if(n == 0 || k == 0 || n < k) {
+			System.out.println(0);
+			return;
+		}
+		
+		long[][] dp = new long[k + 1][n + 1];
+		
+		for(int t = 1; t <= k; t++) {
+			for(int p = 1; p <= n; p++) {
+				if(p < t) {
+					dp[t][p] = 0;
+				} else if(p == t) {
+					dp[t][p] = 1;
+				} else {
+					dp[t][p] = dp[t - 1][p - 1] + dp[t][p - 1] * t;
+				}
+			}
+		}
+		
+		System.out.println(dp[k][n]);
+		
+	}
 }
-
 
                         
 
