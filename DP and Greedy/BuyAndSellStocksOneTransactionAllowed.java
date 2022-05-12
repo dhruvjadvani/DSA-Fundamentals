c2ba7870c206cfbbc2b2a16ee0335355dbb6b05e
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-05-12 22:58:08 +0530

    :)

--- DP and Greedy/BuyAndSellStocksOneTransactionAllowed.java
+++ DP and Greedy/BuyAndSellStocksOneTransactionAllowed.java
@@ -1,30 +1,34 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class BuyAndSellStocksOneTransactionAllowed{
 
    public static void main(String[] args) throws Exception {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      int k = Integer.parseInt(br.readLine());
-
-      if (n == 0 || k == 0 || n < k) {
-         System.out.println(0);
-         return;
-      }
-
-      long[][] dp = new long[k + 1][n + 1];
-      for (int i = 1; i <= k; i++) {
-         for (int j = i; j <= n; j++) {
-            if (i == 1 || j == 1 || i == j) {
-               dp[i][j] = 1;
-            } else {
-               dp[i][j] = dp[i - 1][j - 1] + i * dp[i][j - 1];
-            }
-         }
-      }
-
-      System.out.println(dp[k][n]);
+      Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt(); 
+		
+		int[] prices = new int[n]; 
+		for(int i = 0; i < prices.length; i++) {
+			prices[i] = scn.nextInt();
+		}
+		
+		int leastSoFar = Integer.MAX_VALUE;
+		int overallProfit = 0; 
+		int profitIfSoldToday = 0;
+		
+		
+		for(int i = 0; i < prices.length; i++) {
+			if(prices[i] < leastSoFar) { 
+				leastSoFar = prices[i];
+			}
+			profitIfSoldToday = prices[i] - leastSoFar; 
+			if(profitIfSoldToday > overallProfit) { 
+				overallProfit = profitIfSoldToday;
+			}
+		}
+		
+		
+		System.out.println(overallProfit);
    }
 
 }
