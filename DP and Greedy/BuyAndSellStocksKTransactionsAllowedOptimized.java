ffec953da557c00b0bbe1b1547a5462d4f951fa4
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-05-15 14:04:21 +0530

    :)

--- DP and Greedy/BuyAndSellStocksKTransactionsAllowedOptimized.java
+++ DP and Greedy/BuyAndSellStocksKTransactionsAllowedOptimized.java
@@ -1,1 +1,37 @@
+import java.util.*;
+public class BuyAndSellStocksKTransactionAllowedOptimized {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		
+		int k = scn.nextInt();
+		
+		int[][] dp = new int[k + 1][n];
+		
+		for(int t = 1; t <= k; t++) {
+			int max = Integer.MIN_VALUE;
+			for(int d = 1; d < arr.length; d++) {
+				if(dp[t - 1][d - 1] - arr[d - 1] > max) {
+					max = dp[t - 1][d - 1] - arr[d - 1];
+				}
+				
+				if(max + arr[d] > dp[t][d -1]) {
+					dp[t][d] = max + arr[d];
+				} else { 
+					dp[t][d] = dp[t][d - 1];
+				}
+			}	
+		}
+		
+		
+		System.out.println(dp[k][n - 1]);
+	}
+
+}
 
