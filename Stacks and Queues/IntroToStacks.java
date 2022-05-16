04830aea6af9bcc831c53a778b8cf7b1e3e13c3e
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-05-16 19:40:10 +0530

    :)

--- Stacks and Queues/IntroToStacks.java
+++ Stacks and Queues/IntroToStacks.java
@@ -1,1 +1,25 @@
+import java.util.*;
 
+public class IntroToStacks {
+	public static void main(String[] args) {
+		Stack<Integer> st = new Stack<>();
+		
+		st.push(10);
+		System.out.println(st + "->" + st.peek() + " " + st.size());
+		st.push(20);
+		System.out.println(st + "->" + st.peek() + " " + st.size());
+		st.push(30);
+		System.out.println(st + "->" + st.peek() + " " + st.size());
+		st.push(40);
+		System.out.println(st + "->" + st.peek() + " " + st.size());
+		st.pop();
+		System.out.println(st + "->" + st.peek() + " " + st.size());
+		st.pop();
+		System.out.println(st + "->" + st.peek() + " " + st.size());
+		st.pop();
+		System.out.println(st + "->" + st.peek() + " " + st.size());
+		st.pop();
+		System.out.println(st + "->" +  " " + st.size());
+	}
+
+}
