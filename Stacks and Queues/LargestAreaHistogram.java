2d1f7b91d282a24d8cdadc92da999ccb67663c8b
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-05-22 23:47:03 +0530

    :)

--- Stacks and Queues/LargestAreaHistogram.java
+++ Stacks and Queues/LargestAreaHistogram.java
@@ -36,7 +36,7 @@ public class LargestAreaHistogram {
 	    st.push(0);
 	    lb[0] = -1;
 	    
-	    for(int i = 0; i < arr.length; i++) {
+	    for(int i = 1; i < arr.length; i++) {
 	    	while(st.size() > 0 && arr[i] <= arr[st.peek()]) {
 	    		st.pop();
 	    	}
