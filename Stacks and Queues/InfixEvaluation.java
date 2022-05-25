96673f3e957254b0c328bb3a0309a8cc7ca15e2e
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-05-25 13:20:16 +0530

    :)

--- Stacks and Queues/InfixEvaluation.java
+++ Stacks and Queues/InfixEvaluation.java
@@ -1,85 +1,80 @@
-import java.io.*;
 import java.util.*;
-
-public class Main {
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String exp = br.readLine();
-
-    // code
-    Stack<Integer> operands = new Stack<>();
-    Stack<Character> operators = new Stack<>();
-
-    for (int i = 0; i < exp.length(); i++) {
-      char ch = exp.charAt(i);
-
-      if (ch == '(') {
-        operators.push(ch);
-      } else if (Character.isDigit(ch)) {
-        operands.push(ch - '0');
-      } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-        while (operators.size() > 0 && operators.peek() != '(' && 
-               precedence(ch) <= precedence(operators.peek())) {
-          int val2 = operands.pop();
-          int val1 = operands.pop();
-          char op = operators.pop();
-
-          int opval = operation(val1, val2, op);
-          operands.push(opval);
-        }
-
-        operators.push(ch);
-      } else if (ch == ')') {
-        while (operators.size() > 0 && operators.peek() != '(') {
-          int val2 = operands.pop();
-          int val1 = operands.pop();
-          char op = operators.pop();
-
-          int opval = operation(val1, val2, op);
-          operands.push(opval);
-        }
-
-        if (operators.size() > 0) {
-          operators.pop();
-        }
-      }
-    }
-
-    while (operators.size() > 0) {
-      int val2 = operands.pop();
-      int val1 = operands.pop();
-      char op = operators.pop();
-
-      int opval = operation(val1, val2, op);
-      operands.push(opval);
-    }
-
-    int val = operands.pop();
-    System.out.println(val);
-  }
-
-  public static int precedence(char op){
-    if(op == '+'){
-      return 1;
-    } else if(op == '-'){
-      return 1;
-    } else if(op == '*'){
-      return 2;
-    } else {
-      return 2;
-    }
-  }
-
-  public static int operation(int val1, int val2, char op){
-    if(op == '+'){
-      return val1 + val2;
-    } else if(op == '-'){
-      return val1 - val2;
-    } else if(op == '*'){
-      return val1 * val2;
-    } else {
-      return val1 / val2;
-    }
-  }
+import java.io.*;
+public class InfixEvaluation {
+	public static void main(String[] args) throws Exception {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    String exp = br.readLine();
+	    
+	    Stack<Integer> operands = new Stack<>();
+	    Stack<Character> operators = new Stack<>();
+	    for(int i = 0; i < exp.length(); i++) {
+	    	char ch = exp.charAt(i);
+	    	
+	    	if(ch == '(') {
+	    		operators.push(ch);
+	    	} else if(Character.isDigit(ch)) {
+	    		operands.push(ch - '0'); // converting char to int
+	    	} else if(ch == ')') {
+	    		while(operators.peek() != '(') {
+	    			char operator = operators.pop();
+	    			int v2 = operands.pop();
+	    			int v1 = operands.pop();
+	    			
+	    			int operationValue = operation(v1, v2, operator);
+	    			operands.push(operationValue);
+	    		}
+	    		
+	    		operators.pop();
+	    		
+	    	} else if(ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+	    		while(operators.size() > 0 && operators.peek() != '(' && precedence(ch) <= precedence(operators.peek())) {
+	    			char operator = operators.pop();
+	    			int v2 = operands.pop();
+	    			int v1 = operands.pop();
+	    			
+	    			int operationValue = operation(v1, v2, operator);
+	    			operands.push(operationValue);
+	    		}
+	    		
+	    		operators.push(ch);
+	    	}
+	    }
+	    
+	    while(operators.size() != 0) {
+			char operator = operators.pop();
+			int v2 = operands.pop();
+			int v1 = operands.pop();
+			
+			int operationValue = operation(v1, v2, operator);
+			operands.push(operationValue);
+		}
+	    
+	    System.out.println(operands.peek());
+	    
+	 }
+	
+	public static int precedence(char operator) {
+		if(operator == '+') {
+			return 1;
+		} else if(operator == '-') {
+			return 1;
+		} else if(operator == '*') {
+			return 2;
+		} else {
+			return 2;
+		}
+	}
+	
+	
+	public static int operation(int v1, int v2, char operator) {
+		if(operator == '+') {
+			return v1 + v2;
+		} else if(operator == '-') {
+			return v1 - v2;
+		} else if(operator == '*') {
+			return v1 * v2;
+		} else {
+			return v1 / v2;
+		}
+	}
 }
