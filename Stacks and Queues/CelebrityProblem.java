26257f6dfc2278aa771576ce508fe38b99d08274
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-04 23:46:34 +0530

    :)

--- Stacks and Queues/CelebrityProblem.java
+++ Stacks and Queues/CelebrityProblem.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class CelebrityProblem {
 
     public static void main(String[] args) throws Exception {
         // write your code here
@@ -17,42 +17,38 @@ public class Main {
         }
 
         findCelebrity(arr);
+
     }
 
     public static void findCelebrity(int[][] arr) {
-        // if a celebrity is there print it''s index (not position), if there is not then print "none"
-        Stack < Integer > st = new Stack < > ();
-        for (int i = 0; i < arr.length; i++) {
-            st.push(i);
-        }
-
-        while (st.size() > 1) {
-            int i = st.pop();
-            int j = st.pop();
-
-            if (arr[i][j] == 1) {
-                st.push(j);
-            } else {
-                st.push(i);
-            }
-        }
-
-        int pot = st.pop();
-        boolean flag = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (i != pot) {
-                if (arr[i][pot] == 0 || arr[pot][i] == 1) {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-
-        if (flag) {
-            System.out.println(pot);
-        } else {
-            System.out.println("none");
-        }
+        // if a celebrity is there print it's index (not position), if there is not then
+        // print "none"
+    	Stack<Integer> st = new Stack<>();
+    	for(int i = 0; i < arr.length; i++) {
+    		st.push(i);
+    	}
+    	
+    	while(st.size() >= 2) {
+    		int i = st.pop();
+    		int j = st.pop();
+    		
+    		if(arr[i][j] == 1) {
+    			st.push(j);
+    		} else {
+    			st.push(i);
+    		}
+    	}
+    	
+    	int pot = st.pop();
+    	for(int i = 0; i < arr.length; i++) {
+    		if(i != pot) {
+    			if(arr[i][pot] == 0 || arr[pot][i] == 1) {
+    				System.out.println("none");
+    				return;
+    			}
+    		}
+    	}
+    	System.out.println(pot);
     }
 
 }
