bec14d25f493a857e706dc320034cfde8ee10d3c
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-07 09:44:27 +0530

    :)

--- Stacks and Queues/OOPsSwapGameThree.java
+++ Stacks and Queues/OOPsSwapGameThree.java
@@ -1,1 +1,44 @@
+public class OOPsSwampGameThree {
+	public static class Person { 
+		int age;
+		String name;
+		
+		void saysHi() { 
+			System.out.println(name + "[" + age + "]" + " says hi to you");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Person p1 = new Person(); 
+		p1.age = 10;
+		p1.name = "A";
+		
+		
+		Person p2 = new Person();
+		p2.age = 20;
+		p2.name = "B";
+		
+		
+		p1.saysHi();
+		p2.saysHi();
+		swap3(p1, p2);
+		p1.saysHi();
+		p2.saysHi();
+	}
+	
+	public static void swap3(Person prs1, Person prs2) {
+		prs1 = new Person(); 
+		int temp = prs1.age; 
+		prs1.age = prs2.age; 
+		prs2.age = temp; 
+		
+		prs2 = new Person();
+		String name = prs1.name; 
+		prs1.name = prs2.name; 
+		prs2.name = name;
+		
+	}
+
+}
 
