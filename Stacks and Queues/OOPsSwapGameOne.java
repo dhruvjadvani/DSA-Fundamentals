8999eb35ee5891ecd8dea68eec897f40ecbbf805
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-07 08:45:48 +0530

    :)

--- Stacks and Queues/OOPsSwapGameOne.java
+++ Stacks and Queues/OOPsSwapGameOne.java
@@ -1,1 +1,41 @@
+public class OOPsSwapGameOne {
+	
+	public static class Person { 
+		int age;
+		String name;
+		
+		void saysHi() { 
+			System.out.println(name + "[" + age + "]" + " says hi to you");
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p1 = new Person(); 
+		p1.age = 10;
+		p1.name = "A";
+		
+		
+		Person p2 = new Person();
+		p2.age = 20;
+		p2.name = "B";
+		
+		
+		p1.saysHi();
+		p2.saysHi();
+		swap1(p1, p2);
+		p1.saysHi();
+		p2.saysHi();
+	}
+	
+	//nothing changes 
+	public static void swap1(Person prs1, Person prs2) {
+		Person temp = prs1;
+		prs1 = prs2;
+		prs2 = temp;
+	}
+	
+	
+
+}
 
