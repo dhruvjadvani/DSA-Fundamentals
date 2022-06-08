9fc4707db8aaec9a6d50b474445092b80be47315
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-08 23:12:54 +0530

    :)

--- Stacks and Queues/ConstructorsAndThisEx.java
+++ Stacks and Queues/ConstructorsAndThisEx.java
@@ -1,1 +1,31 @@
+public class ConstructorsAndThisEx {
+	public static class Person {
+		int age;
+		String name;
+		
+		void saysHi() {
+			System.out.println(name + "[" + age + "] says hi");
+		}
+		
+		Person() { //default constructor
+			
+		}
+		
+		Person(int age, String name) { //parameterized constructor
+			this.age= age;
+			this.name = name;
+		}
+	}
+
+	public static void main(String[] args) {
+		Person p1 = new Person(); 
+		p1.age = 10;
+		p1.name = "A";
+		p1.saysHi();
+    
+		Person p2 = new Person(20, "B");
+		p2.saysHi();
+	}
+
+}
 
