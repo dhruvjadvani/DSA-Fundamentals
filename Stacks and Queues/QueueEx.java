ad49956ec7d1ffb0c37400266c1c8c5d25d17fb5
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-15 10:29:27 +0530

    :)

--- Stacks and Queues/QueueEx.java
+++ Stacks and Queues/QueueEx.java
@@ -1,1 +1,32 @@
+import java.util.*;
+public class QueueEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<Integer> que = new ArrayDeque<>();
+		
+		que.add(10);
+		System.out.println(que);
+		
+		que.add(20);
+		System.out.println(que);
+		
+		que.add(30);
+		System.out.println(que);
+		
+		que.add(40);
+		System.out.println(que);
+		
+		que.add(50);
+		System.out.println(que);
+		
+		System.out.println(que.remove());
+		System.out.println(que);
+		
+		System.out.println(que.remove());
+		System.out.println(que);
+		
+	}
+
+}
 
