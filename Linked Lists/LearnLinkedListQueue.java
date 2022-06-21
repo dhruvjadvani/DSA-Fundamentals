0788a63d8f6690bee159163ad628e72e3845cf03
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-21 23:41:35 +0530

    :)

--- Linked Lists/LearnLinkedListQueue.java
+++ Linked Lists/LearnLinkedListQueue.java
@@ -1,1 +1,21 @@
+import java.util.*
+public class LearnLinkedListQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<Integer> queue = new LinkedList<>();
+		
+		queue.offer(12);
+		queue.offer(24);
+		queue.offer(36);
+		queue.offer(48);
+		queue.offer(60);
+		queue.offer(72);
+		
+		System.out.println(queue);
+		
+		System.out.println(queue.poll());
+	}
+
+}
 
