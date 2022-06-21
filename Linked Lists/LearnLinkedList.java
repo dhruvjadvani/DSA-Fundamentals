95e5885e9f48743a51628a72d9bc0dae45f85ffc
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-21 23:49:16 +0530

    :)

--- Linked Lists/LearnLinkedList.java
+++ Linked Lists/LearnLinkedList.java
@@ -1,1 +1,21 @@
+import java.util.*;
+public class LearnLinkedListArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> list = new LinkedList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(list);
+		
+		list.add(4);
+		System.out.println(list);
+		
+		list.add(1, 50);
+		System.out.println(list);
+		
+	}
+
+}
 
