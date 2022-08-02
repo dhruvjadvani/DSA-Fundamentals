2246e5adbe2a2a147bd040194e812db570842b15
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-02 23:34:35 +0530

    Update LinkedListAllOps.java

--- Linked Lists/LinkedListAllOps.java
+++ Linked Lists/LinkedListAllOps.java
@@ -1,1 +1,112 @@
+import java.util.*;
+public class LinkedListAllOps {
+	Node head;
+	private int size;
+	
+	LinkedListAllOps() {
+		size = 0;
+	}
+
+	public class Node {
+		String data;
+		Node next;
+		
+		Node(String data) {
+			this.data = data;
+			this.next = null;
+			size++;
+		}
+	}
+	
+	public void addFirst(String data) {
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public void addLast(String data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+			return;
+		}
+		
+		Node lastNode = head;
+		while(lastNode.next != null) {
+			lastNode = lastNode.next;
+		}
+		
+		lastNode.next = newNode;
+	}
+	
+	public void printList() {
+        Node currNode = head;
+		 
+        while(currNode != null) {
+		   System.out.print(currNode.data+" -> ");
+		   currNode = currNode.next;
+		}
+		 
+		System.out.println("null");
+	}
+	
+	public void removeFirst() {
+	       if(head == null) {
+	           System.out.println("Empty List, nothing to delete");
+	           return;
+	       }
+	 
+	       head = this.head.next;
+	       size--;
+	   }
+	 
+	public void removeLast() {
+	       if(head == null) {
+	           System.out.println("Empty List, nothing to delete");
+	           return;
+	       }
+	 
+	       size--;
+	       if(head.next == null) {
+	           head = null;
+	           return;
+	       }
+	 
+	       Node currNode = head;
+	       Node lastNode = head.next;
+	       
+	       while(lastNode.next != null) {
+	           currNode = currNode.next;
+	           lastNode = lastNode.next;
+	       }
+	 
+	       currNode.next = null;
+	 }
+	 
+	 public int getSize() {
+	    return size;
+	 }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedListAllOps list = new LinkedListAllOps();
+		 list.addLast("is");
+	     list.addLast("a");
+	     list.addLast("list");
+	     list.printList(); // is -> a -> list -> null
+	 
+	     list.addFirst("this");
+	     list.printList(); // this -> is -> a -> list -> null 
+	     System.out.println(list.getSize());  // 4
+	 
+	     list.removeFirst();
+	     
+	     list.printList(); // is -> a -> list -> null
+	     
+	     list.removeLast();
+	     list.printList();	// is -> a -> null
+	}
+
+}
 
