e40f07d16e47618a22184b9ea67acf52ab426ab6
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-02 23:32:46 +0530

    Update LinkedListTemplate.java

--- Linked Lists/LinkedListTemplate.java
+++ Linked Lists/LinkedListTemplate.java
@@ -1,1 +1,19 @@
+public class LinkedListTemplate {
+	public static class Node {
+		int data;
+		Node next;
+	}
+	
+	public static class LinkedListEx {
+		Node head;
+		Node tail;
+		int size;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
 
