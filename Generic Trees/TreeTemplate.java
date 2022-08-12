04e5082bbc1c9516fad3f57c45272b56ef3b2dbb
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-12 15:41:52 +0530

    Update TreeTemplate.java

--- Generic Trees/TreeTemplate.java
+++ Generic Trees/TreeTemplate.java
@@ -1,1 +1,17 @@
+import java.util.*;
+
+public class TreeTemplate {
+	private static class Node {
+		int data;
+		ArrayList<Node> children = new ArrayList<>();
+	}
+	
+	private static class GenericTree { 
+		Node root;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
 
