6809672f5ef3b3197a2d8d04f3044e840c966771
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-12 15:45:18 +0530

    Update DisplayAGenericTree .java

--- Generic Trees/DisplayAGenericTree .java
+++ Generic Trees/DisplayAGenericTree .java
@@ -1,1 +1,50 @@
+import java.util.*;
+
+public class DisplayAGenericTree {
+	private static class Node {
+		int data;
+		ArrayList<Node> children = new ArrayList<>(); 
+	}
+	
+	public static void display(Node node) {
+		String str = node.data + " -> ";
+		for(Node child :  node.children) {
+			str = str + child.data + ", ";
+		}
+		
+		str = str + ".";
+		System.out.println(str);
+		
+		for(Node child: node.children) {
+			display(child);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+		
+		Node root = null; 
+		Stack<Node> st = new Stack<>(); 
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == -1) { 
+				st.pop();
+			} else { 
+				Node temp = new Node(); 
+				temp.data = arr[i]; 
+				
+				if(st.size() > 0) {
+					st.peek().children.add(temp);
+				} else { 
+					root = temp;
+				}
+				
+				st.push(temp); 
+				
+			}
+		}
+		
+		display(root);
+	}
+}
 
