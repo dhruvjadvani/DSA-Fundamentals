6bf82b36694bda4439c58a76070dc32e01563eee
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-08-31 12:06:31 +0530

    Update NodeWithMaximumSubtreeSum.java

--- Generic Trees/NodeWithMaximumSubtreeSum.java
+++ Generic Trees/NodeWithMaximumSubtreeSum.java
@@ -1,1 +1,82 @@
+import java.util.*;
+import java.io.*;
+
+public class NodeWithMaximumSubtreeSum {
+	private static class Node {
+	    int data;
+	    ArrayList<Node> children = new ArrayList<>();
+	  }
+
+	  public static void display(Node node) {
+	    String str = node.data + " -> ";
+	    for (Node child : node.children) {
+	      str += child.data + ", ";
+	    }
+	    str += ".";
+	    System.out.println(str);
+
+	    for (Node child : node.children) {
+	      display(child);
+	    }
+	  }
+
+	  public static Node construct(int[] arr) {
+	    Node root = null;
+
+	    Stack<Node> st = new Stack<>();
+	    for (int i = 0; i < arr.length; i++) {
+	      if (arr[i] == -1) {
+	        st.pop();
+	      } else {
+	        Node t = new Node();
+	        t.data = arr[i];
+
+	        if (st.size() > 0) {
+	          st.peek().children.add(t);
+	        } else {
+	          root = t;
+	        }
+
+	        st.push(t);
+	      }
+	    }
+
+	    return root;
+	  }
+	  
+	  static int maximumSumNode = 0;
+	  static int maximumSum = Integer.MIN_VALUE;
+	  
+	  static int retSumAndCalculateMSST(Node node) {
+		  int sum = 0;
+		  
+		  for(Node child: node.children) {
+			 int childSum = retSumAndCalculateMSST(child);
+			 sum = sum + childSum; 
+		  }
+		  
+		  sum = sum + node.data; 
+		  
+		  if(sum > maximumSum) {
+			  maximumSumNode = node.data;
+			  maximumSum = sum;
+		  }
+		  return sum;
+	  }
+	  
+	  
+	  public static void main(String[] args) throws Exception {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int n = Integer.parseInt(br.readLine());
+	    int[] arr = new int[n];
+	    String[] values = br.readLine().split(" ");
+	    for (int i = 0; i < n; i++) {
+	      arr[i] = Integer.parseInt(values[i]);
+	    }
+
+	    Node root = construct(arr);
+	    retSumAndCalculateMSST(root);
+	    System.out.println(maximumSumNode +"@" + maximumSum);
+	  }
+}
 
